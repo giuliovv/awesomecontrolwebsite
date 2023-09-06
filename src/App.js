3a96d937a3e70ce08a3b7f9ed9f3b0b8c952e2b8
Giulio Vaccari <giulio09@marcov.it>
2023-09-06 19:00:13 +0200

    Search bar

--- src/App.js
+++ src/App.js
@@ -2,48 +2,55 @@ import React from 'react';
 import JSONTable from './JSONTable';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Table, TableHead, TableRow, TableCell, Typography, Link, Box } from '@mui/material';
+import { Table, TableHead, TableRow, TableCell, Typography, Link, Box, TextField } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 function App() {
   const [data, setData] = useState([]);
-  const [uniqueNames, setUniqueNames] = useState(new Set());
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    // Fetch the list of JSON files from the GitHub repository
+    const uniqueNames = new Set(); // Declare uniqueNames inside useEffect
     const repoUrl = 'https://api.github.com/repos/guidosassaroli/controlcompanies/git/trees/main?recursive=1';
     
     axios.get(repoUrl)
       .then(response => {
         const files = response.data.tree.filter(file => file.path.startsWith('data/') && file.path.endsWith('.json'));
         
-        // Download each JSON file and store its contents
         const fetchData = async () => {
           for (const file of files) {
             if (file.path.endsWith('_data_structure.json')) {
               continue;
             }
-        
+
             const fileUrl = `https://raw.githubusercontent.com/guidosassaroli/controlcompanies/main/${file.path}`;
             const fileData = await axios.get(fileUrl);
-      
+
             if (!uniqueNames.has(fileData.data.name)) {
-              // Update the set with new unique name
-              setUniqueNames(prevUniqueNames => new Set([...prevUniqueNames, fileData.data.name]));
-      
-              // Update state with new data
+              uniqueNames.add(fileData.data.name); // Update the local set variable
               setData(prevData => [...prevData, fileData.data]);
             }
           }
         };
 
         fetchData();
-        console.log("Setting data: ", data);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
   }, []);
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredData = data.filter((item) => {
+    return (
+      item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      item.tags.some((tag) => tag.toLowerCase().includes(searchTerm.toLowerCase())) ||
+      item.headquarter.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  });
 
   return (
     <div className="App">
@@ -62,6 +69,16 @@ function App() {
         </Link>
       </Box>
 
+      <Box mb={2}>
+        <TextField
+          label="Search by name, tags or place"
+          variant="outlined"
+          fullWidth
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </Box>
+
       <Table style={{ minWidth: 700 }}>
         <TableHead>
           <TableRow>
@@ -71,7 +88,7 @@ function App() {
             <TableCell style={{ backgroundColor: '#f1f1f1', fontWeight: 'bold' }} align="center">Tags</TableCell>
           </TableRow>
         </TableHead>
-        {data.map((item, index) => (
+        {filteredData.map((item, index) => (
           <JSONTable key={index} data={item} />
         ))}
       </Table>
