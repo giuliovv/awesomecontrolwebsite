2edc936b957169481a73e2915e03d567d2fcc8ce
Giulio Vaccari <giulio09@marcov.it>
2023-09-06 19:05:39 +0200

    Updates

--- precache-manifest.83a5a9ad43df3857e62b608a97d8eb89.js
+++ precache-manifest.83a5a9ad43df3857e62b608a97d8eb89.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "914675d2f172b23b6f4a",
+    "revision": "95a8747e591941afc8fd",
     "url": "/awesomecontrolwebsite/static/css/main.8a14af7f.chunk.css"
   },
   {
-    "revision": "914675d2f172b23b6f4a",
-    "url": "/awesomecontrolwebsite/static/js/main.77671c12.chunk.js"
+    "revision": "95a8747e591941afc8fd",
+    "url": "/awesomecontrolwebsite/static/js/main.60c8657f.chunk.js"
   },
   {
     "revision": "7b4ee0eb4004538600ef",
     "url": "/awesomecontrolwebsite/static/js/runtime~main.a5599daf.js"
   },
   {
-    "revision": "48bb28f6068ae9174fcd",
-    "url": "/awesomecontrolwebsite/static/js/2.bca6426c.chunk.js"
+    "revision": "24d40824d30ae7037e6c",
+    "url": "/awesomecontrolwebsite/static/js/2.ea87b5be.chunk.js"
   },
   {
     "revision": "95aed837b0177e13a1d3",
     "url": "/awesomecontrolwebsite/static/js/3.99af2e10.chunk.js"
   },
   {
-    "revision": "e3bbc7c015ba23b5d0945050cdb4a02e",
+    "revision": "4c75b43ca25231fa687724e7bcc4dcf1",
     "url": "/awesomecontrolwebsite/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/awesomecontrolwebsite/precache-manifest.71b79f0886445fe84d482d4b51bed153.js"
+  "/awesomecontrolwebsite/precache-manifest.83a5a9ad43df3857e62b608a97d8eb89.js"
 );
 
 workbox.clientsClaim();
