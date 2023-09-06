f5a3e1b1cc4a624fb293deb6e293e4d6a62ad2af
Giulio Vaccari <giulio09@marcov.it>
2023-09-06 19:08:29 +0200

    Link validation

--- src/JSONTable.js
+++ src/JSONTable.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { TableBody, TableCell, TableRow, Link } from '@mui/material';
 
 export default function JSONTable({ data }) {
+  const url = data.url;
+
+  // If url doesn't start with 'http' or 'https', prepend 'http://'
+  const validURL = url.startsWith('http://') || url.startsWith('https://') ? url : `http://${url}`;
+
   return (
     <TableBody>
       <TableRow>
         <TableCell align="center">
-          {data.url ? (
-            <Link href={data.url} target="_blank" rel="noopener">
-              {data.name}
-            </Link>
-          ) : (
-            data.name
-          )}
+          <Link href={validURL} target="_blank" rel="noopener noreferrer">
+            {data.name}
+          </Link>
         </TableCell>
         <TableCell align="center">{data.headquarter}</TableCell>
         <TableCell align="center">{data.short_description}</TableCell>
