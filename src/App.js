7c3932565ff1710fdbffd3db548174e973cd6db6
Giulio Vaccari <giulio09@marcov.it>
2023-09-06 18:45:20 +0200

    Nice working version

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import JSONTable from './JSONTable';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Table, TableHead, TableRow, TableCell } from '@mui/material';
+import { Table, TableHead, TableRow, TableCell, Typography, Link, Box } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function App() {
   const [data, setData] = useState([]);
+  const [uniqueNames, setUniqueNames] = useState(new Set());
 
   useEffect(() => {
     // Fetch the list of JSON files from the GitHub repository
@@ -18,15 +20,25 @@ function App() {
         // Download each JSON file and store its contents
         const fetchData = async () => {
           for (const file of files) {
+            if (file.path.endsWith('_data_structure.json')) {
+              continue;
+            }
+        
             const fileUrl = `https://raw.githubusercontent.com/guidosassaroli/controlcompanies/main/${file.path}`;
             const fileData = await axios.get(fileUrl);
-            
-            // Update the state as each file arrives
-            setData(prevData => [...prevData, fileData.data]);
+      
+            if (!uniqueNames.has(fileData.data.name)) {
+              // Update the set with new unique name
+              setUniqueNames(prevUniqueNames => new Set([...prevUniqueNames, fileData.data.name]));
+      
+              // Update state with new data
+              setData(prevData => [...prevData, fileData.data]);
+            }
           }
         };
 
         fetchData();
+        console.log("Setting data: ", data);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
@@ -35,6 +47,21 @@ function App() {
 
   return (
     <div className="App">
+      <Box display="flex" flexDirection="column" alignItems="center" mb={4}>
+        <Typography variant="h3" gutterBottom align="center">
+          Awesome Control Companies
+        </Typography>
+
+        <Link href="https://github.com/guidosassaroli/controlcompanies/tree/main" target="_blank" rel="noopener">
+          <Box display="flex" alignItems="center">
+            <GitHubIcon />
+            <Typography variant="body1" style={{ marginLeft: 8 }}>
+              View on GitHub
+            </Typography>
+          </Box>
+        </Link>
+      </Box>
+
       <Table style={{ minWidth: 700 }}>
         <TableHead>
           <TableRow>
