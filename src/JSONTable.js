a6bfd46ab814a2eb57801e69f70eb1b5682c8f93
Giulio Vaccari <giulio09@marcov.it>
2023-09-06 19:05:06 +0200

    WIth links

--- src/JSONTable.js
+++ src/JSONTable.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { TableBody, TableCell, TableRow } from '@mui/material';
+import { TableBody, TableCell, TableRow, Link } from '@mui/material';
 
 export default function JSONTable({ data }) {
   return (
     <TableBody>
       <TableRow>
-        <TableCell align="center">{data.name}</TableCell>
+        <TableCell align="center">
+          {data.url ? (
+            <Link href={data.url} target="_blank" rel="noopener">
+              {data.name}
+            </Link>
+          ) : (
+            data.name
+          )}
+        </TableCell>
         <TableCell align="center">{data.headquarter}</TableCell>
         <TableCell align="center">{data.short_description}</TableCell>
         <TableCell align="center">{data.tags.join(', ')}</TableCell>
