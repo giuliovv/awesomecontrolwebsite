2310b14e95c16c75716bfaa5a4e10ee7283b496c
Giulio Vaccari <giulio09@marcov.it>
2023-09-06 18:43:37 +0200

    Updates

---
+++ precache-manifest.cfb5dc650265aaa0184c08100e2eebb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "285e46e68c025ff47b61",
+    "url": "/awesomecontrolwebsite/static/css/main.8a14af7f.chunk.css"
+  },
+  {
+    "revision": "285e46e68c025ff47b61",
+    "url": "/awesomecontrolwebsite/static/js/main.ee67dd81.chunk.js"
+  },
+  {
+    "revision": "8b158652678fe5cc09e2",
+    "url": "/awesomecontrolwebsite/static/js/runtime~main.93e5f8dd.js"
+  },
+  {
+    "revision": "03c6d481c65908605a36",
+    "url": "/awesomecontrolwebsite/static/js/2.d59822cc.chunk.js"
+  },
+  {
+    "revision": "283fe33f7690b8d03a74",
+    "url": "/awesomecontrolwebsite/static/js/3.d0bba312.chunk.js"
+  },
+  {
+    "revision": "85853e8c6ad5f7ddb047f9d44cc93959",
+    "url": "/awesomecontrolwebsite/index.html"
+  }
+];
